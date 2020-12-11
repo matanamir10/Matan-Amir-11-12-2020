3c2c181d257c84195b7df3a35600edcf761d67ac
Matan Amir <matan6080@gmail.com>
2020-12-11 14:43:58 +0200

    routing basic functionally was added

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam
-      earum porro nihil tenetur a soluta, quia quo, commodi sapiente minima
-      inventore reiciendis animi laboriosam. Minima provident suscipit
-      consequuntur non distinctio expedita. A, voluptate expedita doloremque
-      repellendus veritatis ab excepturi ipsa voluptates fuga ratione quibusdam
-      facilis sed, dolorum quo impedit maiores dolore magni quaerat?
-    </div>
+    <Switch>
+      <Route component={() => <h1>This works</h1>} />
+    </Switch>
   );
 };
