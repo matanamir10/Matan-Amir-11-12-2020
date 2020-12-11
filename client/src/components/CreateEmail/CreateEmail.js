7a842f603ea3fb3cf1982641cb82b8a4af20a447
Matan Amir <matan6080@gmail.com>
2020-12-11 15:29:33 +0200

    added paper to form and button disable state

--- client/src/components/CreateEmail/CreateEmail.js
+++ client/src/components/CreateEmail/CreateEmail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CreateEmail.scss';
+import { Button, Paper } from '@material-ui/core';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Input } from '../../UI/Input';
@@ -25,53 +26,66 @@ export const CreateEmail = () => {
     },
   });
   return (
-    <form className='create-email__form' noValidate autoComplete='off'>
-      <Input
-        label='Subject'
-        id='subject'
-        name='subject'
-        type='text'
-        error={formik.touched.subject && formik.errors.subject}
-        errorMessage={formik.errors.subject}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.subject}
-      />
-      <Input
-        label='Message'
-        id='message'
-        name='message'
-        type='text'
-        error={formik.touched.message && formik.errors.message}
-        errorMessage={formik.errors.message}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.message}
-        rows={4}
-        rowsMax={8}
-      />
-      <Input
-        label='Sender Id'
-        id='senderId'
-        name='senderId'
-        type='text'
-        error={formik.touched.senderId && formik.errors.senderId}
-        errorMessage={formik.errors.senderId}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.senderId}
-      />
-      <Input
-        label='Recciver Id'
-        id='recciverId'
-        name='recciverId'
-        type='text'
-        error={formik.touched.recciverId && formik.errors.recciverId}
-        errorMessage={formik.errors.recciverId}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.recciverId}
-      />
-    </form>
+    <Paper className='create-email' elevation={5}>
+      <form className='create-email__form' noValidate autoComplete='off'>
+        <Input
+          className='create-email__input'
+          label='Subject'
+          id='subject'
+          name='subject'
+          type='text'
+          error={formik.touched.subject && formik.errors.subject}
+          errorMessage={formik.errors.subject}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.subject}
+        />
+        <Input
+          className='create-email__input'
+          label='Message'
+          id='message'
+          name='message'
+          type='text'
+          error={formik.touched.message && formik.errors.message}
+          errorMessage={formik.errors.message}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.message}
+          rows={4}
+          rowsMax={8}
+        />
+        <Input
+          className='create-email__input'
+          label='Sender Id'
+          id='senderId'
+          name='senderId'
+          type='text'
+          error={formik.touched.senderId && formik.errors.senderId}
+          errorMessage={formik.errors.senderId}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.senderId}
+        />
+        <Input
+          className='create-email__input'
+          label='Recciver Id'
+          id='recciverId'
+          name='recciverId'
+          type='text'
+          error={formik.touched.recciverId && formik.errors.recciverId}
+          errorMessage={formik.errors.recciverId}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.recciverId}
+        />
+        <Button
+          type='submit'
+          variant='contained'
+          color='primary'
+          disabled={!formik.isValid}>
+          Send
+        </Button>
+      </form>
+    </Paper>
   );
 };
