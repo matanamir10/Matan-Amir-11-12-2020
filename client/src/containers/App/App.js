75d57cdd7ab2961c0421753f71523cc3c91f151b
Matan Amir <matan6080@gmail.com>
2020-12-11 14:42:12 +0200

    dummy changes

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export const App = () => {
-  return <div>APp</div>;
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam
+      earum porro nihil tenetur a soluta, quia quo, commodi sapiente minima
+      inventore reiciendis animi laboriosam. Minima provident suscipit
+      consequuntur non distinctio expedita. A, voluptate expedita doloremque
+      repellendus veritatis ab excepturi ipsa voluptates fuga ratione quibusdam
+      facilis sed, dolorum quo impedit maiores dolore magni quaerat?
+    </div>
+  );
 };
