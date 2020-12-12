40f2ac624e8d0ad9767bcfa62adbe316c28a3ab8
Matan Amir <matan6080@gmail.com>
2020-12-12 13:02:51 +0200

    addede dotenv for configuration

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectToDB = () => {
   return new Promise((resolve, reject) => {
     mongoose
-      .connect('mongodb://localhost:27017/Messages', {
+      .connect(process.env.MONGO_URI!, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
+import * as dotenv from 'dotenv';
 import { connectToDB } from './db/mongoose';
 
+dotenv.config();
+// if checks to verify env vairbales declared
+
 const run = async () => {
+  console.log(process.env.NODE_ENV);
   await connectToDB();
   console.log('connected to mongodb');
 };
