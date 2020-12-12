c462c3c059d84f19412627857a825fb3f4ef62b0
Matan Amir <matan6080@gmail.com>
2020-12-12 18:07:19 +0200

    some refactors logic

--- client/src/containers/ManageEmail/ManageEmail.js
+++ client/src/containers/ManageEmail/ManageEmail.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './ManageEmail.scss';
+import { MessageDetails } from './MessageDetails/MessageDetails';
 import { EmailDetails } from '../../components/EmailDetails/EmailDetails';
 
 const ManageEmail = () => {
   return (
-    <>
+    <div className='manage-details'>
       <EmailDetails />
-    </>
+      <MessageDetails />
+    </div>
   );
 };
 export default ManageEmail;
