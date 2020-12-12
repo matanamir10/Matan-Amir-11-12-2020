d45cae9504bdb2601383a9173e1472a9679e66ea
Matan Amir <matan6080@gmail.com>
2020-12-12 16:32:30 +0200

    added lazy loading for routes

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import './App.scss';
 import { Route, Switch } from 'react-router-dom';
-import { ComposeEmail } from '../ComposeEmail/ComposeEmail';
+import { CircularProgress } from '@material-ui/core';
+import { Navigation } from '../../components/Navigation/Navigation';
+
+const ComposeEmail = React.lazy(() => import('../ComposeEmail/ComposeEmail'));
+const ManageEmail = React.lazy(() => import('../ManageEmail/ManageEmail'));
 
 export const App = () => {
   return (
-    <Switch>
-      <Route component={() => <ComposeEmail />} />
-    </Switch>
+    <div className='container'>
+      <Navigation />
+      <div className='content'>
+        <Suspense fallback={<CircularProgress color='secondary' />}>
+          <Switch>
+            <Route path='/compose' component={ComposeEmail} />
+            <Route path='/manage' component={ManageEmail} />
+          </Switch>
+        </Suspense>
+      </div>
+    </div>
   );
 };
