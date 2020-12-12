707b3a9a660a18dfbefe08c43d20ba6774707c34
Matan Amir <matan6080@gmail.com>
2020-12-12 16:34:46 +0200

    added support for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import './config/axios';
 import { App } from './containers/App/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/storeConfig';
+
+console.log(process.env.NODE_ENV);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
