f3e40d99f8d6c71f97176a7aefbcb5a0a9707b9b
Matan Amir <matan6080@gmail.com>
2020-12-12 19:21:13 +0200

    Action route added

--- client/src/components/MessageList/MessageItem.js
+++ client/src/components/MessageList/MessageItem.js
@@ -6,8 +6,15 @@ import {
   IconButton,
 } from '@material-ui/core';
 import { Delete, Info } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 
 export const MessageItem = ({ message, prefix }) => {
+  const history = useHistory();
+  const routeToMessageInfo = () => {
+    history.push(`${history.location.pathname}/messageInfo/${message.id}`, {
+      message,
+    });
+  };
   return (
     <ListItem>
       <ListItemText
@@ -18,7 +25,7 @@ export const MessageItem = ({ message, prefix }) => {
         <IconButton edge='end' aria-label='delete'>
           <Delete />
         </IconButton>
-        <IconButton edge='end' aria-label='delete'>
+        <IconButton edge='end' aria-label='delete' onClick={routeToMessageInfo}>
           <Info />
         </IconButton>
       </ListItemSecondaryAction>
