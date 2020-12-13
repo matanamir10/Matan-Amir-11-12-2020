1b08e9c733730edbaef48e7e9de197e9c429e96d
Matan Amir <matan6080@gmail.com>
2020-12-13 17:19:42 +0200

    check for rnv variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,16 @@ import { connectToDB } from './db/mongoose';
 import { ServerApp } from './server';
 
 dotenv.config();
-// if checks to verify env vairbales declared
 
+if (!process.env.MONGO_URI || !process.env.JWT_KEY) {
+  throw new Error('MONGO_URI and JWT_KEY must be provided');
+}
 const run = async () => {
   await connectToDB();
-  const port = 4000;
+  const port = 4000 || process.env.PORT;
   new ServerApp().start(port);
 };
 
 run().catch((err) => {
-  console.log(err);
+  process.exit(0);
 });
