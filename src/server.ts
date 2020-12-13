c01c2029745dae8625f14c13db43e008cc7d4d18
Matan Amir <matan6080@gmail.com>
2020-12-13 22:06:53 +0200

    Fixes for production

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ import 'express-async-errors';
 import { Server } from '@overnightjs/core';
 import cors from 'cors';
 import cookieSession from 'cookie-session';
-import { NotFoundError } from './errors/notFound';
-import { currentUser } from './middlewares/current-user';
 import { errorHandler } from './middlewares/errorHandler';
 import { AuthController } from './controllers/auth';
 import { MessageController } from './controllers/message';
@@ -20,23 +18,22 @@ export class ServerApp extends Server {
       cookieSession({
         keys: ['secret'],
         signed: false,
-        // secure: process.env.NODE_ENV !== 'test',
         secure: false,
         httpOnly: true,
       })
     );
-    if (process.env.NODE_ENV === 'production') {
-      this.app.use(express.static(path.join(__dirname, 'client', 'build')));
-    }
     this.setupControllers();
-
+    if (process.env.NODE_ENV === 'production') {
+      this.app.use(
+        express.static(path.join(__dirname, '..', 'client', 'build'))
+      );
+      this.app.get('/*', (req, res) => {
+        res.sendFile(
+          path.join(__dirname, '..', 'client', 'build', 'index.html')
+        );
+      });
+    }
     this.app.use(errorHandler);
-    this.app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-    // this.app.all('*', async () => {
-    //   throw new NotFoundError();
-    // });
   }
 
   private setupControllers(): void {
